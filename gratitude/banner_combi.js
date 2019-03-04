c94d0c82e4177d0d14403868684a827dbfd671ee
Max Klein <isalix@gmail.com>
2019-03-04 12:45:18 -0800

    make left to right work somewhat

--- gratitude/banner_combi.js
+++ gratitude/banner_combi.js
@@ -7,7 +7,8 @@ import { getLocalizations } from './l10ns';
 $( document ).ready( function () {
 	const bannerName = 'CS18_grat_combi';
 	const bannerTemplate = require( './templates/banner01.hbs' );
-	const dF = getDecidingFactors();
+	let dF = getDecidingFactors();
+	dF.siteLang = 'fa';
     let bannerEnabled = decideShow( dF.siteLang, dF.userReg, dF.userEditCount,
 		dF.userGroups );
     bannerEnabled = true;
--- gratitude/l10ns.js
+++ gratitude/l10ns.js
@@ -1,9 +1,9 @@
 export function getLocalizations( lang ) {
-	const l10ns = { en: { bannerTitleText: 'Learn about wikipedia. this is going to be a lot of text, align automatically.', bannerSubtitleText: 'CivilServant presents', bannerButtonText: 'click here to learn more' },
-		ar: { bannerTitleText: 'ساهم في بيئة أفضل للمشاركة الويكيبيديّة العربية!', bannerSubtitleText: 'كن جزء من دراسة عن تحسين بيئة المشاركة الويكيبيديّة العربية!', bannerButtonText: 'للمزيد من المعلومات' },
-		de: { bannerTitleText: 'Hilf mit, die Wikipedia zu erforschen!', bannerSubtitleText: 'Nimm an einer Studie teil, die die Beteiligung in der Wikipedia untersucht.', bannerButtonText: 'Mehr erfahren' },
-		fa: { bannerTitleText: 'برای یک ویکی‌پدیای دوستانه‌تر', bannerSubtitleText: 'در یک تحقیق برای بهبود جامعه ویکی‌پدیای فارسی شرکت کنید. برای کسب اطلاعات بیشتر اینجا کلیک کنید.', bannerButtonText: 'اطلاعات بیشت' },
-		pl: { bannerTitleText: 'Wesprzyj rozwój Wikipedii!', bannerSubtitleText: 'Weź udział w badaniu naukowym, które pomoże naszej społeczności stać się bardziej życzliwą', bannerButtonText: 'Dowiedz się więcej' }
+	const l10ns = { en: { bannerTitleQuestion: 'Learn about wikipedia. this is going to be a lot of text, align automatically.', bannerSubtitleAsk: 'CivilServant presents', bannerButtonNext: 'click here to learn more→', bannerDir: 'ltr' },
+		ar: { bannerTitleQuestion: 'ساهم في بيئة أفضل للمشاركة الويكيبيديّة العربية!', bannerSubtitleAsk: 'كن جزء من دراسة عن تحسين بيئة المشاركة الويكيبيديّة العربية!', bannerButtonNext: 'للمزيد من المعلومات←', bannerDir: 'rtl' },
+		de: { bannerTitleQuestion: 'Hilf mit, die Wikipedia zu erforschen!', bannerSubtitleAsk: 'Nimm an einer Studie teil, die die Beteiligung in der Wikipedia untersucht.', bannerButtonNext: 'Mehr erfahren→', bannerDir: 'ltr' },
+		fa: { bannerTitleQuestion: 'برای یک ویکی‌پدیای دوستانه‌تر', bannerSubtitleAsk: 'در یک تحقیق برای بهبود جامعه ویکی‌پدیای فارسی شرکت کنید. برای کسب اطلاعات بیشتر اینجا کلیک کنید.', bannerButtonNext: 'اطلاعات بیشت←', bannerDir: 'rtl' },
+		pl: { bannerTitleQuestion: 'Wesprzyj rozwój Wikipedii!', bannerSubtitleAsk: 'Weź udział w badaniu naukowym, które pomoże naszej społeczności stać się bardziej życzliwą', bannerButtonNext: 'Dowiedz się więcej→', bannerDir: 'ltr' }
 	};
 
 	return l10ns[ lang ];
